955c8727099a67590d464522c82b6589e93b10de
macBook Pro <mac@macBooks-MacBook-Pro.local>
2024-03-20 20:13:57 +0000

    sypress project

---
+++ cypress/integration/manager/addBank.spec.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login')
+  })
+
+
+  it('go to login ', () => {
+    cy.get(':nth-child(3) > .btn').click()
+    cy.get('[ng-class="btnClass1"]').click()
+    cy.get(':nth-child(1) > .form-control').type('Ayoub')
+    cy.get(':nth-child(2) > .form-control').type('Ellaouzi')
+    cy.get(':nth-child(3) > .form-control').type('Full stack dev')
+    cy.get('form.ng-dirty > .btn').click()
+//-----------------------open acount-----------------------------------------
+
+cy.get('[ng-class="btnClass2"]').click()
+
+cy.get('#userSelect').select('Ayoub Ellaouzi');
+cy.get('#currency').select('Dollar');
+cy.get('form.ng-dirty > button').click()
+//-----------------------check if added-----------------------------------------
+ cy.get('[ng-class="btnClass3"]').click()
+ cy.get('table tbody tr:last-child td:nth-child(1)').should('contain', 'Ayoub');
+//-------------------------delete---------------------------------------
+cy.get('table tbody tr:last-child td:nth-child(5)> button').click()
+//-----------------------check if added-----------------------------------------
+cy.get('table tbody tr:last-child td:nth-child(1)').should('not.contain', 'Ayoub');
+
+
+
+// cy.get('[ng-class="btnClass3"]').click()
+// cy.get('.form-control').type('Ayoub')
+
+
+    //
+
+  })
+
+  // it('go to add customer ', () => {
+    
+  // })
+
+})
